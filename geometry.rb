6786fcf5d8d4cd63d4cc613f66c210be90668404
vickyalsua <victoria.alsua@gmail.com>
2020-12-15 10:10:46 -0300

    cambios de fede

--- geometry.rb
+++ geometry.rb
@@ -1,8 +1,8 @@
 require "./rectangle.rb"
-# require "./circle.rb"
-# require "./square.rb"
+require "./circle.rb"
+require "./square.rb"
 
 
 puts Rectangle.area(3,4)
-# puts Circle.area(3)
-# puts Square.area(3,4)
+puts Circle.area(3)
+puts Square.area(3,4)
